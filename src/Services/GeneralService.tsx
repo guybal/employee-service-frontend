1fc1fab4c667fe813c7c2391a850a98c2d1e7b95
Guy Balmas <guyb@terasky.com>
2023-08-03 00:18:34 +0300

    + added files

--- src/Services/GeneralService.tsx
+++ src/Services/GeneralService.tsx
@@ -3,7 +3,8 @@ import EmployeeModel from "../Models/EmployeeModel";
 
 class GeneralService {
 
-    private url = "http://localhost:8080/api/";
+    private url = "http://search-api.apps.svc.cluster.local/api/";
+//     private url = "http://search.guy-tkg-tap1-cls.terasky.demo:8080/api/";
 
     public async getAllEmployees () : Promise<EmployeeModel[]>{
         const response = await axios.get<EmployeeModel[]>( this.url + "getAllEmployee");
